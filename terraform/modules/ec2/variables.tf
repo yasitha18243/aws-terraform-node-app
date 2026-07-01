39fc0f2dfcd3e61e5f0cb597b8d86674c715df40
yasitha18243 <yasitha.herath.se@gmail.com>
2026-07-01 20:50:05 +1000

    Removed ssh access to the ec2 from security group

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -7,11 +7,11 @@ variable "public_subnet_ids" {
 variable "app_bucket_arn" {}
 variable "app_bucket_name" {}
 variable "instance_type" {}
-variable "ssh_allowed_cidr" {
-  description = "CIDR block allowed to SSH into EC2 instances"
-  type        = string
-  default     = "103.53.117.233/32"
-}
+# variable "ssh_allowed_cidr" {
+#   description = "CIDR block allowed to SSH into EC2 instances"
+#   type        = string
+#   default     = "103.53.117.233/32"
+# }
 variable "key_name" {
   type        = string
   description = "Name of the existing EC2 key pair to use for SSH access"
